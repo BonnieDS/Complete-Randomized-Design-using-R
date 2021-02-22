cdc3035b97803ec2a812f2cfff985c2500fe3bb0
BonnieDS <72938726+BonnieDS@users.noreply.github.com>
2021-02-22 16:58:36 +0300

    Update CRD.R

--- CRD.R
+++ CRD.R
@@ -1,7 +1,8 @@
+#Load this package using the function library
 library(readxl)
-
+#Read the file from your files location as below.
 CRD <- read_excel("C:/Users/Boss IT Solutions/OneDrive/CRD.xlsx", col_types = c("text", "numeric"))
-
+#Display the data in the file CRD.xlsx
 View(CRD)
 
 #Fitting of linear model
